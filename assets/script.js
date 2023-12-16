a7628978097cc80e46efbd364a895942784069a8
mmorrow6109 <msmorrow6109@gmail.com>
2023-12-16 12:41:24 -0800

    Yelp JS done.  Need to finish Translate

--- assets/script.js
+++ assets/script.js
@@ -4,90 +4,116 @@ const searchBtn = document.getElementById('search-btn');
 const reservationForm = document.getElementById('form');
 const dateInput = document.getElementById('date');
 const submitBtn = document.getElementById('submit-btn');
-
+const translateApiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+const translateApiKey = '3f877754a2mshf4074c4dd98fe98p1bbe81jsn7911bc7ba389';
 
 searchBtn.addEventListener('click', function (event) {
-  event.preventDefault();
-  
-  const query = searchInput.value;
-  
-  // Yelp API endpoint
-  let apiUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=portland&price=1,2,3,4&term=${query}&limit=4`;
-  
-  // Yelp API request options
-  const options = {
-    headers: {
-      "accept": "application/json",
-      "x-requested-with": "xmlhttprequest",
-      'Access-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${apiKey}`,
-    },
-    method: 'GET',
-  };
+    event.preventDefault();
 
-  // Make the API request
-  fetch(apiUrl, options)
-  .then(response => response.json())
-  .then(data => {
-    // Display the data in the HTML
-    console.log(data)
-    displayData(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error('Error:', error);
-  });
-  
+    const query = searchInput.value;
+
+    let apiUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=portland&price=1,2,3,4&term=${query}&limit=20`;
+
+    const options = {
+        headers: {
+            "accept": "application/json",
+            "x-requested-with": "xmlhttprequest",
+            'Access-Control-Allow-Origin': '*',
+            Authorization: `Bearer ${apiKey}`,
+        },
+        method: 'GET',
+    };
+    const resultDiv = document.getElementById('result');
+
+    fetch(apiUrl, options)
+        .then(response => {
+            resultDiv.classList.remove('hidden');
+
+            return response.json();
+        })
+
+        .then(data => {
+            const resultEls = [
+                document.getElementById('result1'),
+                document.getElementById('result2'),
+                document.getElementById('result3'),
+                document.getElementById('result4'),
+                document.getElementById('result5'),
+                document.getElementById('result6'),
+                document.getElementById('result7'),
+                document.getElementById('result8'),
+                document.getElementById('result9'),
+                document.getElementById('result10'),
+                document.getElementById('result11'),
+                document.getElementById('result12'),
+                document.getElementById('result13'),
+                document.getElementById('result14'),
+                document.getElementById('result15'),
+                document.getElementById('result16'),
+                document.getElementById('result17'),
+                document.getElementById('result18'),
+                document.getElementById('result19'),
+                document.getElementById('result20'),
+            ];
+
+            for (let i = 0; i < data.businesses.length; i++) {
+                const business = data.businesses[i];
+                const resultEl = resultEls[i];
+
+                if (resultEl) {
+                    const img = resultEl.querySelector('img');
+                    if (img) {
+                        img.src = business.image_url;
+                    }
+
+                    const p = resultEl.querySelector('p');
+                    if (p) {
+                        p.textContent = business.name;
+                    }
+
+                    createButtons(resultEl, business);
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
 
-function displayData(data) {
-  const resultsDiv = document.getElementById('results');
-  resultsDiv.innerHTML = '';
-  data.businesses.forEach(business => {
-    const businessButton = document.createElement('button');
-    businessButton.textContent = business.name;
-    businessButton.onclick = function() {
-      // Show the modal
-      const modal = document.getElementById('modal');
-      modal.style.display = 'block';
+function createButtons(element, business) {
+    element.onclick = function () {
+        // Show the modal
+        const modal = document.getElementById('modal');
+        modal.style.display = 'block';
 
-      // Update the modal content with the business data
-      const modalContent = document.getElementById('modal-content');
-      modalContent.innerHTML = '';
+        // Update the modal content with the business data
+        const modalContent = document.getElementById('modal-content');
+        modalContent.innerHTML = '';
 
-      let name = business.name;
-      let price = business.price;
-      let rating = business.rating;
-      let phone = business.phone;
-      let reviewCount = business.review_count;
+        const bizList = document.createElement('ul');
 
-      const bizList = document.createElement('ul');
+        const nameItem = document.createElement('li');
+        nameItem.textContent = `Name: ${business.name}`;
+        bizList.appendChild(nameItem);
 
-      const nameItem = document.createElement('li');
-      nameItem.textContent = `Name: ${name}`;
-      bizList.appendChild(nameItem);
+        const priceItem = document.createElement('li');
+        priceItem.textContent = `Price: ${business.price}`;
+        bizList.appendChild(priceItem);
 
-      const priceItem = document.createElement('li');
-      priceItem.textContent = `Price: ${price}`;
-    bizList.appendChild(priceItem);
+        const ratingItem = document.createElement('li');
+        ratingItem.textContent = `Rating: ${business.rating}`;
+        bizList.appendChild(ratingItem);
 
-    const ratingItem = document.createElement('li');
-    ratingItem.textContent = `Rating: ${rating}`;
-    bizList.appendChild(ratingItem);
+        const phoneItem = document.createElement('li');
+        phoneItem.textContent = `Phone: ${business.phone}`;
+        bizList.appendChild(phoneItem);
 
-    const phoneItem = document.createElement('li');
-    phoneItem.textContent = `Phone: ${phone}`;
-    bizList.appendChild(phoneItem);
+        const reviewCountItem = document.createElement('li');
+        reviewCountItem.textContent = `Reviews: ${business.review_count}`;
+        bizList.appendChild(reviewCountItem);
 
-    const reviewCountItem = document.createElement('li');
-    reviewCountItem.textContent = `Reviews: ${reviewCount}`;
-    bizList.appendChild(reviewCountItem);
-
-    modalContent.appendChild(bizList);
-       
+        modalContent.appendChild(bizList);
     };
-    resultsDiv.appendChild(businessButton);
-  });
 }
 
 // Add an event listener to the submit button
@@ -145,3 +171,21 @@ const closeButton = document.getElementById('close');
 closeButton.onclick = function() {
 modal.style.display = 'none';
 };
+
+function translateText(text, targetLang) {
+  const url = `${translateApiUrl}&q=${encodeURIComponent(text)}&target=${targetLang}`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Extract the translated text
+      const translatedText = data.translations[0].translatedText;
+
+      // Use the translated text wherever needed
+      // (e.g., update UI elements, send translated data)
+      console.log(`Translated "${text}" to "${targetLang}": ${translatedText}`);
+    })
+    .catch(error => {
+      console.error('Error translating text:', error);
+    });
+}
